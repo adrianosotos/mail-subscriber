014247f180b74bcbb41b9cc8c2f95429d762a732
Adriano <adriano.sotos@linx3.com>
2021-07-29 22:39:00 -0300

    feat: themes

---
+++ client/src/styles/theme.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  background: '#ebebff',
+  form: '#ffffff',
+  fontColor: '#000000',
+  moonColor: 'grey',
+  sunColor: 'yellow'
+}
+  
+export const darkTheme = {
+  background: '#292929',
+  form: '#000000',
+  fontColor: '#ffffff',
+  moonColor: 'yellow',
+  sunColor: 'grey'
+}
